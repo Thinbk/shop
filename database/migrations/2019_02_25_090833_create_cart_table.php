b6e98d652b2b311e212b198df4b01b9f2049abdd
Nguyen Dang Thin <thinnd@ominext.com>
2019-02-27 17:23:12 +0700

    first commit

---
+++ database/migrations/2019_02_25_090833_create_cart_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quatity');
+            $table->string('product_name');
+            $table->float('product_price');
+            $table->integer('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
